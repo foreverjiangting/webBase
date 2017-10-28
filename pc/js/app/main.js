496c4f9ffa00abf82b7e333ed469f6c044d29ee5
jiang.ting <jiang.ting@Ctrip.com>
2017-10-28 16:51:11 +0800

    fix

--- pc/js/app/main.js
+++ pc/js/app/main.js
@@ -17,11 +17,21 @@ lab.loadpage = function(id, pageName, _this) {
         core.loadHtml(id, pageName);
     });
 }
-lab.isShow = function(showClassName, hideClassName, _this) {
-    $(".m_card").find("li").removeClass("relation");
+lab.isShow = function(ulClassName, showClassName, hideClassName, _this) {
+    $("." + ulClassName).find("li").removeClass("relation");
     $(_this).parent("li").addClass("relation");
     $("." + showClassName).css("display", "none");
     $("." + hideClassName).css("display", "block");
+};
+
+lab.showPanel = function(ulClassName, _this) {
+    var index = $(_this).attr("data-index");
+    $("." + ulClassName).find("li").removeClass("relation");
+    $(_this).parent("li").addClass("relation");
+    //展现面板
+    $(".content-indent").css("display", "none");
+    $("." + index).css("display", "block");
+    console.log(index);
 };
 // 运行
 $(function() {
